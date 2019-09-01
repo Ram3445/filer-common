d4c0e3f881c09b18e45b39baf9650a0270c8740b
Ramkumar <ramkumars.mailme@gmail.com>
2019-09-01 16:00:13 -0400

    initial version

---
+++ src/main/java/com/file/microservsample/common/FilerCommonApplication.java
@@ -0,0 +1,13 @@
+package com.file.microservsample.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilerCommonApplication.class, args);
+	}
+
+}
